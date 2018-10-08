0c0caa0e3cc55895b6da24faa2de91adf92127ef
Stefan Reiser <stefan.reiser@zenner-iot.com>
2018-10-09 00:53:39 +0200

    added readme and example

---
+++ examples/test.py
@@ -0,0 +1,26 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+from elementapi import ElementAPI
+
+api = ElementAPI("<your api key>", baseurl='stage.element-iot.com')
+
+print("TAGS\n"+16*'-')
+for t in api.tags():
+   print(t)
+
+print("DEVICES\n"+16*'-')
+for d in api.devices(limit=10, filter={'slug': None, 'updated_at': None}):
+    print(d)
+
+
+print("INTERFACES\n"+16*'-')
+for i in api.interfaces('<your device uuid>'):
+    print(i)
+
+print("PACKETS\n"+16*'-')
+for p in api.packets('<your device uuid>', limit=10):
+    print(p)
+
+print("READINGS\n"+16*'-')
+for r in api.readings('<your device uuid>', limit=10):
+    print(r)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# element-iot api client for python
+simple api client that supports auto resolving pages and simple filters. uses requests lib to fetch data from the element-iot REST API.
