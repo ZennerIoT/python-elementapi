5c06c3585e2425aaa5f67d1a25d03055040a1664
Stefan Reiser <stefan.reiser@zenner-iot.com>
2019-02-12 10:26:52 +0100

    cosmetics

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 
 setup(
-  name = 'elementapi',
-  packages = ['elementapi'],
-  version = '0.1',
-  description = 'element-iot api client lib',
-  author = 'Stefan Reiser',
-  author_email = 'sr@zenner-iot.com',
-  url = 'https://github.com/ZennerIoT/python-elementapi',
-  download_url = 'https://github.com/ZennerIoT/python-elementapi/archive/master.zip',
-  keywords = ['client', 'rest', 'element-iot'],
-  classifiers = [
+  name='elementapi',
+  packages=['elementapi'],
+  version='0.2',
+  description='element-iot api client lib',
+  author='Stefan Reiser',
+  author_email='sr@zenner-iot.com',
+  url='https://github.com/ZennerIoT/python-elementapi',
+  download_url='https://github.com/ZennerIoT/python-elementapi/archive/master.zip',
+  keywords=['client', 'rest', 'element-iot'],
+  classifiers =[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
