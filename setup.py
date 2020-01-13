6659fc49bca3397b9ad3f86d911d4aa1d73f35cd
Stefan Reiser <stefan.reiser@zenner-iot.com>
2020-01-13 12:42:04 +0100

    - raise version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name='elementapi',
   packages=['elementapi'],
-  version='0.3.2',
+  version='0.3.3',
   description='element-iot api client lib',
   author='Stefan Reiser',
   author_email='sr@zenner-iot.com',
